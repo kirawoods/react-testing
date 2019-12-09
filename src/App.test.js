5027d7a228f790b23f85bfcdcf5684ebe4877209
Kira <kawoods826@gmail.com>
2019-12-09 09:03:11 -0800

    added basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { italic } from "ansi-colors";
+import expectExport from "expect";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(rtl.cleanup);
+
+it("renders without crashing", () => {
+  const wrapper = rtl.render(<span className="greet">hello world</span>);
+  const element = wrapper.queryByText(/hello/i);
+  expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
 });
